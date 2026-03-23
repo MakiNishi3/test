6c84ec26bb321ce6b9c6b9544b1cc8609c80f00d
Maki Nishi <tantamoraru49@gmail.com>
2026-03-23 22:06:56 +0200

    Create ohaahaah.wasm.js

---
+++ ohaahaah/ohaahaah.wasm.js
@@ -0,0 +1,11 @@
+try {
+  throw new WebAssembly.CompileError("Hello", "someFile", 10);
+} catch (e) {
+  console.log(e instanceof CompileError); // true
+  console.log(e.message); // "Hello"
+  console.log(e.name); // "CompileError"
+  console.log(e.fileName); // "someFile"
+  console.log(e.lineNumber); // 10
+  console.log(e.columnNumber); // 0
+  console.log(e.stack); // returns the location where the code was run
+}
